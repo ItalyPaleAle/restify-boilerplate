975c1566893f82e6675c64da6cd66b4f82516faa
Alessandro Segala <alessandro.segala@gmail.com>
2014-04-04 17:12:51 -0400

    Supporting SSL

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+var fs = require('fs')
 var restify = require('restify')
 
 // Load environments
 var config = require(__dirname + '/lib/environment')()
 
 var server = restify.createServer({
+	
+	// Uncomment to enable SSL
+	/*certificate: fs.readFileSync('ssl/ssl-cert.pem'),
+	key: fs.readFileSync('ssl/ssl-key.pem'),*/
+	
 	name: 'restify-boilerplate',
 	version: '1.0.0'
 })
+
 server.use(restify.acceptParser(server.acceptable))
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
